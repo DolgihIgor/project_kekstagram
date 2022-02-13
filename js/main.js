553de8620a2f689a3357d67975f6b0f092d8a6fb
Igor Dolgikh <igor_dolgih@inbox.ru>
2022-02-13 20:17:00 +0700

    Написал функцию определения длины строки

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Функция для получения случайного числа в заданно диапазоне
 const getRandomInt = (min, max) => {
   if (min < 0 || max < 0) {
     return -1;
@@ -8,4 +9,9 @@ const getRandomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
+// Функция определения длины строки
+const stringCount = (string, sign) => {
+  return text.length <= sign ? true : false;
+}
+console.log(getRandomInt(1,30));
 console.log(getRandomInt(1,30));
