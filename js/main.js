c7d306e3b6dc6860e451b368306dea61003ed488
DolgihIgor <igor_dolgih@inbox.ru>
2022-02-14 15:49:44 +0700

    Весь проект

--- js/main.js
+++ js/main.js
@@ -1,17 +1,66 @@
-// Функция для получения случайного числа в заданно диапазоне
-const getRandomInt = (min, max) => {
-  if (min < 0 || max < 0) {
-    return -1;
-  };
-  if (max < min) {
-    [min, max] = [max, min]
-  };
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+import { renderPhotos } from './addpreview.js';
+import { request } from './fetch.js';
+import { showError } from './alerts.js';
+import { shuffleArray, debounce } from './util.js';
+import './editor.js';
+import './effects.js';
+import './validation.js';
+import './load-image.js';
 
-// Функция определения длины строки
-const stringCount = (string, sign) => {
-  return text.length <= sign ? true : false;
+const DEFAULT_PREVIEW_LOAD = 25;
+const RANDOM_PREVIEW_LOAD = 10;
+
+const filter = document.querySelector('.img-filters');
+let photos = [];
+
+function removeActiveClass() {
+  let activeFilter = document.querySelector('.img-filters__button--active');
+  activeFilter.classList.remove('img-filters__button--active');
 }
-console.log(getRandomInt(1,30));
-console.log(getRandomInt(1,30));
+
+const removePhotos = () => {
+  const images = document.querySelectorAll('.picture');
+  if (images) {
+    images.forEach(element => {
+      element.remove();
+    });
+  }
+}
+
+const filters = {
+  'filter-default': () => {
+    renderPhotos(photos.slice(0, DEFAULT_PREVIEW_LOAD))
+  },
+  'filter-random': () => {
+    renderPhotos(shuffleArray(photos.slice()).slice(0, RANDOM_PREVIEW_LOAD));
+
+  },
+  'filter-discussed': () => {
+    renderPhotos(photos.slice().sort((a, b) => {
+      return b.comments.length - a.comments.length;
+    }))
+  },
+}
+
+const onSuccess = (data) => {
+  filter.classList.remove('img-filters--inactive');
+  photos = data.slice()
+  renderPhotos(photos.slice(0, DEFAULT_PREVIEW_LOAD))
+}
+
+const onError = () => {
+  showError('Ошибка загрузки, попробуйте еще раз', 'Закрыть')
+}
+
+request(onSuccess, onError, 'GET')
+
+const onFilterClick = debounce((evt) => {
+  if (evt.target.classList.contains('img-filters__button')) {
+    removeActiveClass();
+    removePhotos();
+    evt.target.classList.add('img-filters__button--active')
+    filters[evt.target.id]()
+  }
+})
+
+filter.addEventListener('click', onFilterClick)
